2b2b5be642fde9a672de0d059d75d69f520dcf8a
EMI <3lo.a3o@gmail.com>
2021-10-07 13:51:07 +0000

    PHP/Laravel 10 ControllerとViewが連携できるようにしよう 課題４

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,5 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
 });
     Route::get('XXX', 'AAAController@bbb');
-　　
-    
-    Route::get('admin/profile/create', 'Admin\ProfileController@add');
-    Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+Route::get('admin/profile/create', 'Admin\ProfileController@add');
+Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
