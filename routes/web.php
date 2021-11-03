cf9dc0236fabd792f0134280d8633ee18b1fd54d
EMI <3lo.a3o@gmail.com>
2021-11-03 06:33:01 +0000

    (課題)PHP/Laravel 13 ニュース投稿画面を作成しよう

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,11 @@ Route::get('/', function () {
 });
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
     Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('profile/edit', 'Admin\ProfileController@update');
+    
 });
     Route::get('XXX', 'AAAController@bbb');
 Auth::routes();
